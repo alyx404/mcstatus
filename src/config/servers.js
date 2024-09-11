02fd7ba8bfb03ea1554e84b72fa9983b7f00bb71
Alyx404 <24651806+alyx404@users.noreply.github.com>
2024-09-12 00:15:18 +0800

    Update servers.js

--- src/config/servers.js
+++ src/config/servers.js
@@ -1,30 +1,10 @@
 export const servers = [
 	{
-		"title": "Popular Servers",
+		"title": "Portals",
 		"servers": [
 			{
-				"name": "The Chunk",
-				"host": "play.chunkmc.com",
-				"port": 25565
-			},
-			{
-				"name": "Hypixel",
-				"host": "mc.hypixel.net",
-				"port": 25565
-			},
-			{
-				"name": "Minehut",
-				"host": "minehut.com",
-				"port": 25565
-			},
-			{
-				"name": "Wynncraft",
-				"host": "play.wynncraft.com",
-				"port": 25565
-			},
-			{
-				"name": "CubeCraft",
-				"host": "play.cubecraft.net",
+				"name": "wiaf.top",
+				"host": "wiaf.top",
 				"port": 25565
 			}
 		]
