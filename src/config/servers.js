cc5b187ff466151fe0b932fb3bfc673c162f3ea4
Alyx404 <24651806+alyx404@users.noreply.github.com>
2025-01-16 09:10:28 +0800

    Update servers.js

--- src/config/servers.js
+++ src/config/servers.js
@@ -1,11 +1,21 @@
 export const servers = [
 	{
-		"title": "Portals",
+		"title": "Wiaf",
 		"servers": [
 			{
 				"name": "wiaf.top",
 				"host": "wiaf.top",
 				"port": 25565
+			}
+		]
+	},
+	{
+		"title": "AlyxCraft",
+		"servers": [
+			{
+				"name": "mc.alyx.link",
+				"host": "36.34.23.37",
+				"port": 63074
 			}
 		]
 	}
